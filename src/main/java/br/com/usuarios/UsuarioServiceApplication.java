d5ccfb664200cc60aa65faa221255c459997c48a
cunhatalisca <gabrielvlog11@gmail.com>
2025-06-03 21:26:48 -0300

    fix: testando a resolução de alguns erros

---
+++ src/main/java/br/com/usuarios/UsuarioServiceApplication.java
@@ -0,0 +1,14 @@
+
+package br.com.usuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class UsuarioServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UsuarioServiceApplication.class, args);
+    }
+}
---
+++ src/main/java/br/com/usuarios/config/SecurityConfig.java
@@ -0,0 +1,38 @@
+package br.com.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+	    http
+	        .csrf(csrf -> csrf.disable()) // Desativa CSRF
+	        .authorizeHttpRequests(auth -> auth
+	            /*.requestMatchers(HttpMethod.POST, "/usuarios").authenticated()
+	            .requestMatchers(HttpMethod.POST, "/usuarios/login").authenticated()
+	            .requestMatchers(HttpMethod.GET, "/usuarios").authenticated()	*/            
+	            .anyRequest().permitAll()
+	            
+	        )
+				//.httpBasic(withDefaults()); // Mantém autenticação Basic
+			;
+
+	    return http.build();
+	}
+
+	//Criptografia de senhas
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+}
